dee6068baeb0fad67a772cb87703b2c38676578f
Tulasi Narayana Dattu Suggala <tulasi.narayana@infinite.com>
2023-02-28 14:02:16 +0530

    added googleMap

---
+++ src/elements/Map/styles.js
@@ -0,0 +1,20 @@
+import { styled } from "@mui/material/styles";
+
+export const mapContainerStyles = {
+  width: "100%",
+  height: "100%",
+};
+
+export const mapStyleWidth = {
+  width: "100%",
+  height: "100%",
+};
+
+export const RootContainer = styled("div")`
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+`;
--- src/pages/DashBoard/dashBoard.tsx
+++ src/pages/DashBoard/dashBoard.tsx
@@ -1,11 +1,11 @@
-import { useState, useEffect } from "react";
-import Profile from '../Profile';
+import { useState, useEffect, Fragment } from "react";
+import { Grid, Box } from "@mui/material";
 import theme from "../../theme/theme";
 import useTranslation from "../../localization/translations";
+import Map from "elements/Map";
 import useStyles from "./styles";
 
 const DashBoard = () => {
-
   const { hello } = useTranslation();
 
   const [selectedTheme, setSelectedTheme] = useState(
@@ -37,11 +37,19 @@ const DashBoard = () => {
   }, [selectedTheme]);
 
   return (
-    <>
-      Dashboard 
-       {/* {hello}
-      <Profile /> */}
-    </>
+    <Fragment>
+      <Box>
+        <Grid container>
+          <Grid item xs={12} sm={12} md={8} lg={9} xl={9}>
+            <Map />
+          </Grid>
+
+          <Grid item xs={12} sm={12} md={4} lg={3} xl={3}>
+            Equipments
+          </Grid>
+        </Grid>
+      </Box>
+    </Fragment>
   );
 };
 export default DashBoard;
