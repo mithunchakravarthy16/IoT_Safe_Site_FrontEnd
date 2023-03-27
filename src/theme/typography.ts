7d94b19c76e1c724833b15db6f1a19fac34f1b8d
Mithun Chakravarthy <mithunch@infinite.com>
2023-03-27 19:01:21 +0530

    local storage retrival

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -1,6 +1,57 @@
 // import { createTheme } from "@mui/material/styles";
 
+
+
 // const theme = createTheme();
+
+const tags = [
+  {
+    name: "H1",
+    size: "2",
+    color: "#FFFFFF",
+  },
+];
+
+const buttons = [
+  {
+    name: "Primary",
+    size: "2",
+    bgColor: "#FFFFFF",
+    textColor: "#FFFFFF",
+  },
+];
+
+const markers = [
+  {
+    name: "Events",
+    bgColor: "#FFFFFF",
+  },
+];
+
+const tabs = [
+  {
+    name: "Events",
+    bgColor: "#FFFFFF",
+    textColor: "#FFFFFF",
+  },
+];
+
+
+
+  const data = JSON.parse(localStorage.getItem("colorScheme") || "{}");
+
+console.log("typo-data-from-local", data?.semanticTags)
+
+  if (
+    data?.buttons?.length > 0 ||
+    data?.markers?.length > 0 ||
+    data?.semanticTags?.length > 0 ||
+    data?.tabs?.length > 0
+  ) {
+    
+  } else {
+    
+  }
 
 
 
