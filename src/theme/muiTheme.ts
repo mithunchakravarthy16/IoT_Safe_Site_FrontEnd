ae27f0796af7065552ddcd665d58abff3531bb30
Shanmukh kumar Alla <kumara@infinite.com>
2023-03-27 17:56:10 +0530

    Theme typography changes

--- src/theme/muiTheme.ts
+++ src/theme/muiTheme.ts
@@ -1,7 +1,9 @@
 import { createTheme } from "@mui/material/styles";
 import breakpoints from "./breakpoints";
+import typography from "./typography";
 
 const muiTheme = createTheme();
 muiTheme.breakpoints = { ...muiTheme.breakpoints, ...breakpoints };
+muiTheme.typography = { ...muiTheme.typography, ...typography };
 
 export default muiTheme;
---
+++ src/theme/typography.ts
@@ -0,0 +1,40 @@
+// import { createTheme } from "@mui/material/styles";
+
+// const theme = createTheme();
+
+
+
+const h1 = {  
+  fontSize: "24px",
+  color:''
+};
+
+const h2 = { 
+};
+
+const h3 = { 
+};
+
+const h4 = {
+};
+
+const h5 = {
+};
+
+const h6 = {};
+
+
+const body = {};
+
+
+const typography = { 
+  h1: h1,
+  h2: h2,
+  h3: h3,
+  h4: h4,
+  h5: h5,
+  h6: h6,
+  body: body
+};
+
+export default typography;
