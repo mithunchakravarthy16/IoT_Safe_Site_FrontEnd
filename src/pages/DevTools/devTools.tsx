eea34c5f6adb2edd7dd333650a40f6793ad664b6
Shibi Chakkaravarthy <shibi.chakkaravarthy@infinite.com>
2023-03-28 19:46:43 +0530

    added custom logo and text functionality in devTools

--- src/pages/DevTools/devTools.tsx
+++ src/pages/DevTools/devTools.tsx
@@ -1,6 +1,7 @@
-import { useState, Fragment, useEffect } from "react";
+import { useState, Fragment, useEffect, useRef } from "react";
 import { Grid } from "@mui/material";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import AdminHeader from "components/AdminHeader";
 import ColorPicker from "elements/ColorPicker";
 import Radio from "@mui/material/Radio";
@@ -12,6 +13,7 @@ import Select from "../../elements/Select";
 import adminPlusIcon from "../../assets/admin-plus-icon.svg";
 import deleteIcon from "../../assets/trashIcon.svg";
 import useStyles from "./styles";
+import default_logo from "../../assets/default_logo.svg";
 
 const DevTools = () => {
   const {
@@ -25,6 +27,7 @@ const DevTools = () => {
     adminHeaderButtonSection,
     updateButton,
     colorSchemeHeading,
+    colorSchemeSubeading,
     innerPanel,
     backgroundColor,
     radioButtonHeader,
@@ -36,9 +39,22 @@ const DevTools = () => {
     radioButton,
     deleteIconClass,
     insideContainer,
+    fileUploadContainer,
+    fileUploadTitle,
+    fileUploadContent,
+    logoPreviewWrapper,
+    logoPreviewInnercontainer,
+    logoPreview,
+    invisibleDisplay,
   } = useStyles();
 
+  const headerLogoInputRef = useRef<any>(null)
+  const loginLogoInputRef = useRef<any>(null)
+  const footerLogoInputRef = useRef<any>(null)
+
   const [activePage, setActivePage] = useState<any>();
+  const [footerLogoType, setFooterLogoType] = useState("image");
+  const [customLogos, setCustomLogos] = useState({header: "", login: "", footer: {type: "image", value: "", color: ""}})
   const tags = [
     {
       name: "H1",
@@ -277,34 +293,104 @@ const DevTools = () => {
     localStorage.setItem("colorScheme", JSON.stringify(data));
   };
 
-  return (
-    <Fragment>
-      <Grid container>
-        <Grid item xs={12}>
-          <AdminHeader />
-        </Grid>
-      </Grid>
-      <Grid container className={adminContentPanel}>
-        <Grid item xs={12} sm={12} md={2} lg={2} xl={2}>
-          <div className={adminLeftPanel}>
-            {menuItems &&
-              menuItems.length > 0 &&
-              menuItems.map((menu: any, index: any) => {
-                return (
-                  <p
-                    key={index}
-                    className={
-                      activePage === menu.id ? menuItemListActive : menuItemList
-                    }
-                    onClick={(event) => handleClick(event, menu.id)}
-                  >
-                    {menu.name}
-                  </p>
-                );
-              })}
-          </div>
-        </Grid>
-        <Grid item xs={12} sm={12} md={10} lg={10} xl={10}>
+  const handleLogoChangesUpdate = () => {
+    localStorage.setItem("customLogos", JSON.stringify(customLogos))
+  }
+
+  const getLogos = () => {
+    const defaultLogos = JSON.stringify({
+      login: "",
+      header: "",
+      footer: {
+        type: "image",
+        value: ""
+      }
+    })
+    const logos = JSON.parse(localStorage.getItem("customLogos") || defaultLogos)
+    setCustomLogos(logos)
+  }
+
+  const onLogoChange = (place: string) => {
+    switch(place) {
+      case 'header': {
+        headerLogoInputRef?.current?.click()
+        break;
+      }
+      case 'login': {
+        loginLogoInputRef?.current?.click()
+        break;
+      }
+      case 'footer': {
+        footerLogoInputRef?.current?.click()
+        break;
+      }
+    }
+  }
+
+  const onLogoUpdated = (event: any, location: string) => {
+    const oldLogos = {...customLogos};
+    let imageString = ""
+
+    const file = event.target.files[0];
+    const mimeType = file.type
+
+  // Create a new file reader
+    const reader: any = new FileReader();
+
+    // Set the onload event handler
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      // Convert the file contents to base64
+      const base64String = reader.result.replace('data:', '').replace(/^.+,/, '');
+
+      // Log the base64 string to the console
+      imageString = "data:"+mimeType+";base64,"+base64String;
+
+      switch(location) {
+        case "header": {
+          oldLogos.header = imageString
+          break;
+        }
+        case "login": {
+          console.log("INSIDE CASE LOGIN", imageString)
+          oldLogos.login = imageString
+          break;
+        }
+        case "footer": {
+          oldLogos.footer.value = imageString
+          oldLogos.footer.type = footerLogoType
+          break;
+        }
+      }
+
+      setCustomLogos(oldLogos)
+      console.log("ALL TASKS DONE", oldLogos, location)
+    };
+  }
+
+  const onFooterTextChange = (evt: any) => {
+    const oldLogos = {...customLogos};
+
+    oldLogos.footer.value = evt.target.value
+    oldLogos.footer.type = footerLogoType
+
+    setCustomLogos(oldLogos)
+  }
+
+  const onFooterTextColorChange = (evt: any) => {
+    const oldLogos = {...customLogos};
+
+    oldLogos.footer.color = evt.target.value
+    oldLogos.footer.type = footerLogoType
+
+    setCustomLogos(oldLogos)
+  }
+
+  const pageContentRenderer = () => {
+    switch(activePage) {
+      case 0: {
+        return (
+          <Grid item xs={12} sm={12} md={10} lg={10} xl={10}>
           <div className={adminRightPanel}>
             <div className={innerPanel}>
               <Grid container className={adminRightPanelHeader}>
@@ -658,6 +744,146 @@ const DevTools = () => {
             </div>
           </div>
         </Grid>
+        )
+      }
+
+      case 1: {
+        return (
+          <Grid item xs={12} sm={12} md={10} lg={10} xl={10}>
+            <div className={adminRightPanel}>
+              <div className={innerPanel}>
+                <Grid container className={adminRightPanelHeader}>
+                  <Grid item xs={6}>
+                    <p className={colorSchemeHeading}>Logo</p>
+                    <p className={colorSchemeSubeading} >Allowed file extension * (.jpeg, .png, .svg). Max file size 5 MB.</p>
+                  </Grid>
+                  <Grid item xs={6} className={adminHeaderButtonSection}>
+                    <Button variant="outlined" className={previewButton}>
+                      Preview
+                    </Button>
+                    <Button variant="contained" className={updateButton} disabled>
+                      Cancel
+                    </Button>
+                    <Button
+                      variant="contained"
+                      className={updateButton}
+                      onClick={handleLogoChangesUpdate}
+                    >
+                      Update
+                    </Button>
+                  </Grid>
+                </Grid>
+                <div className={insideContainer}>
+                  <div className={fileUploadContainer} >
+                    <div className={fileUploadTitle} >Login</div>
+                    <div className={fileUploadContent} >
+                      <div className={logoPreviewWrapper} >
+                        <div className={logoPreviewInnercontainer} >
+                          <img className={logoPreview} src={customLogos.login || default_logo} />
+                        </div>
+                      </div>
+                      <Button variant="contained" className={updateButton} onClick={() => onLogoChange("login")}>
+                        Change
+                      </Button>
+                    </div>
+                  </div>
+                  <div className={fileUploadContainer} >
+                    <div className={fileUploadTitle} >Header</div>
+                    <div className={fileUploadContent} >
+                      <div className={logoPreviewWrapper} >
+                        <div className={logoPreviewInnercontainer} >
+                          <img className={logoPreview} src={customLogos.header || default_logo} />
+                        </div>
+                      </div>
+                      <Button variant="contained" className={updateButton} onClick={() => onLogoChange("header")}>
+                        Change
+                      </Button>
+                    </div>
+                  </div>
+                  <div className={fileUploadContainer} >
+                    <div className={fileUploadTitle} >Footer</div>
+                    <RadioGroup
+                      row
+                      aria-labelledby="demo-row-radio-buttons-group-label"
+                      name="row-radio-buttons-group"
+                      value={footerLogoType}
+                      onChange={evt => setFooterLogoType((evt.target as HTMLInputElement).value)}
+                    >
+                      <FormControlLabel value="image" control={<Radio />} label="Image" />
+                      <FormControlLabel value="text" control={<Radio />} label="Text" />
+                    </RadioGroup>
+                    {
+                      footerLogoType === "image"
+                      ?
+                      <div className={fileUploadContent} >
+                        <div className={logoPreviewWrapper} >
+                          <div className={logoPreviewInnercontainer} >
+                            <img className={logoPreview} src={customLogos.footer.value || default_logo} />
+                          </div>
+                        </div>
+                        <Button variant="contained" className={updateButton} onClick={() => onLogoChange("footer")}>
+                          Change
+                        </Button>
+                      </div>
+                      :
+                      <div className={fileUploadContent} >
+                        <Grid container className={adminRightPanelBackgroundColor}>
+                          <Grid item>
+                            <p className={radioButtonHeader}>Exter Text</p>
+                            <div className={colorPickerItem}>
+                              <TextField id="outlined-basic" label="Enter Text" variant="outlined" onChange={onFooterTextChange} value={customLogos.footer.value} />
+                            </div>
+                          </Grid>
+                          <Grid item>
+                            <p className={radioButtonHeader}>Change Text Color</p>
+                            <div className={colorPickerItem}>
+                              <ColorPicker onChange={onFooterTextColorChange} value={customLogos.footer.color} />
+                            </div>
+                          </Grid>
+                        </Grid>
+                      </div>
+                    }
+                  </div>
+                  <input type="file" className={invisibleDisplay} ref={headerLogoInputRef} onChange={(evt: any) => onLogoUpdated(evt, "header")} />
+                  <input type="file" className={invisibleDisplay} ref={loginLogoInputRef} onChange={(evt: any) => onLogoUpdated(evt, "login")} />
+                  <input type="file" className={invisibleDisplay} ref={footerLogoInputRef} onChange={(evt: any) => onLogoUpdated(evt, "footer")} />
+                </div>
+              </div>
+            </div>
+          </Grid>
+        )
+      }
+    }
+  }
+
+  return (
+    <Fragment>
+      <Grid container>
+        <Grid item xs={12}>
+          <AdminHeader />
+        </Grid>
+      </Grid>
+      <Grid container className={adminContentPanel}>
+        <Grid item xs={12} sm={12} md={2} lg={2} xl={2}>
+          <div className={adminLeftPanel}>
+            {menuItems &&
+              menuItems.length > 0 &&
+              menuItems.map((menu: any, index: any) => {
+                return (
+                  <p
+                    key={index}
+                    className={
+                      activePage === menu.id ? menuItemListActive : menuItemList
+                    }
+                    onClick={(event) => handleClick(event, menu.id)}
+                  >
+                    {menu.name}
+                  </p>
+                );
+              })}
+          </div>
+        </Grid>
+        {pageContentRenderer()}
       </Grid>
     </Fragment>
   );
